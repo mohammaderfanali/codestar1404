d46243759ec3361a61805d67a768340fda843522
Sorooshsatary <sorooshsatary@gmail.com>
2025-08-18 10:53:07 +0330

    efcore project done

---
+++ faz8/efcore/Students/Database/StudentScore.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Students.Database;
+
+[Table("studentscores")]
+public class StudentScore
+{
+    [Column("studentnumber")]
+    public int StudentNumber { get; set; }
+
+    [Column("lesson")]
+    public string Lesson { get; set; } = string.Empty;
+
+    [Column("score")]
+    public double Score { get; set; }
+}
