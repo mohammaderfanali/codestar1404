e5ca242a5dba1bf4d69ac077c3affd55cccdcd0e
Sorooshsatary <sorooshsatary@gmail.com>
2025-09-13 09:46:02 +0330

    removing obj and bin

--- project/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ project/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e517bf1def24f4ac2a12cbc3e46e7c06814a67ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0a7d8d3040ecba97bf25ae946e271fe35cc68e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
